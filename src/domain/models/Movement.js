239eab447b27a4df1597503dfc861fd9f7a00d09
Alexandre <borges_2.0@hotmail.com>
2020-11-11 22:42:31 -0300

    Model de movimentações

---
+++ src/domain/models/Movement.js
@@ -0,0 +1,37 @@
+const { MOVING_TYPES } = require('../../constants');
+
+/**
+ * @param {{ mongoose: import('mongoose') }} param
+ */
+exports.MovementFactory = ({ mongoose } = {}) => {
+  const MovementSchema = new mongoose.Schema({
+    paymentRecord: { type: mongoose.Schema.Types.ObjectId, required: true, index: { unique: true } },
+    movingDate: { type: Date, required: true },
+    movingType: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return MOVING_TYPES.map((a) => a.id).includes(v);
+        },
+      },
+    },
+    movingValue: {
+      originalValue: { type: Number, required: true },
+      interest: { type: Number, require: true },
+      trafficTicket: { type: Number, required: true },
+    },
+    transaction: {
+      type: String,
+      required: true,
+      index: { unique: true },
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }).index({ paymentRecord: 1, transaction: 1 });
+  const Movement = mongoose.model('Movement', MovementSchema);
+
+  return { Movement };
+};
