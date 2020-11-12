15302b82452ca0582103ef1f655476a35f9e85cf
Alexandre <borges_2.0@hotmail.com>
2020-11-11 23:44:30 -0300

    Ajustes nas constantes

--- src/constants.js
+++ src/constants.js
@@ -1,26 +1,43 @@
-exports.EVENT_TYPES = Object.freeze({
+exports.EVENT_PAY_TYPE = Object.freeze({
   PURCHASE: 1,
   SERVICES: 2,
   EMPLOYEEPAYMENT: 3,
   REPLACEMENT: 4,
 });
 
-exports.STATE_TYPES = Object.freeze({
+exports.EVENT_PAY_TYPES = Object.values(this.EVENT_PAY_TYPE);
+
+exports.EVENT_RECEIVE_TYPE = Object.freeze({
+  SALES: 1,
+  SERVICES: 2,
+  REPLACEMENT_NEGOTIATION: 3,
+  REPLACEMENT_CREDITCARD: 4,
+});
+
+exports.EVENT_RECEIVE_TYPES = Object.values(this.EVENT_RECEIVE_TYPE);
+
+exports.STATE_TYPE = Object.freeze({
   OPEN: 1,
   CANCELED: 2,
   SETTLED: 3,
   SUBSTITUTED: 4,
 });
 
-exports.ACCOUNT_TYPES = Object.freeze({
+exports.STATE_TYPES = Object.values(this.STATE_TYPE);
+
+exports.ACCOUNT_TYPE = Object.freeze({
   PAY: 1,
   RECEIVE: 2,
 });
 
-exports.MOVING_TYPES = Object.freeze({
+exports.ACCOUNT_TYPES = Object.values(this.ACCOUNT_TYPE);
+
+exports.MOVING_TYPE = Object.freeze({
   OPENING: 1,
   SETTLEMENT_TOTAL: 2,
   SETTLEMENT_PARTIAL: 3,
   CANCELLATION: 4,
   REPLACEMENT: 5,
 });
+
+exports.MOVING_TYPES = Object.values(this.MOVING_TYPE);
